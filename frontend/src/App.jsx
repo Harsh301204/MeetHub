9587fdfdc6164ec7e867d600e863bb4ee42946a7
Harsh Sharma <harshsharma301a@gmail.com>
2025-07-16 18:22:30 +0530

    added routing for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,38 @@
 import React from 'react'
-import { Route, Routes } from 'react-router'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import SignUpPage from './pages/SignUpPage'
-import NotificationsPage from './pages/NotificationsPage'
-import ChatPage from './pages/ChatPage'
-import CallPage from './pages/CallPage'
-import OnBoardingPage from './pages/OnBoardingPage'
+import { Navigate, Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import NotificationsPage from './pages/NotificationsPage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+import CallPage from './pages/CallPage.jsx'
+import OnBoardingPage from './pages/OnBoardingPage.jsx'
 import toast , { Toaster } from 'react-hot-toast'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios' 
+import { axiosInstance } from './lib/axios.js'
 
 const App = () => {
+  const {data:authData , isLoading , error} = useQuery({queryKey : ["authUser"] , queryFn : async () => {
+    const res = await axiosInstance.get('/auth/me')
+    return res.data;
+  } , retry : false
+})
+
+  const authUser = authData?.user
+  
+
   return (
     <div className='h-screen' data-theme = "night">
       <button onClick={() => toast.success("HEllo world")}>Create a Toast</button>
       <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/signup' element={<SignUpPage/>} />
-        <Route path='/login' element={<LoginPage/>} />
-        <Route path='/notifications' element={<NotificationsPage/>} />
-        <Route path='/call' element={<CallPage/>} />
-        <Route path='/chat' element={<ChatPage/>} />
-        <Route path='/onboard' element={<OnBoardingPage/>} />
+        <Route path='/' element={authUser ? <HomePage/> : <Navigate to={'/login'}/>} />
+        <Route path='/signup' element={!authUser ? <SignUpPage/> : <Navigate to={'/'}/>} />
+        <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to={'/'}/>} />
+        <Route path='/notifications' element={authUser ? <NotificationsPage/> : <Navigate to={'/login'}/>} />
+        <Route path='/call' element={authUser ? <CallPage/> : <Navigate to={'/login'}/>} />
+        <Route path='/chat' element={authUser ? <ChatPage/> : <Navigate to={'/login'}/>} />
+        <Route path='/onboard' element={authUser ? <OnBoardingPage/> : <Navigate to={'/login'}/>} />
       </Routes>
 
       <Toaster/>

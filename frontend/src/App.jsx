a37aaec50abfa2cc6a709d5715e4ec32d6343b28
Harsh Sharma <harshsharma301a@gmail.com>
2025-07-18 16:01:36 +0530

    Added ThemeSelector Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,9 +15,11 @@ import PageLoader from './components/PageLoader.jsx'
 import { getAuthUser } from './lib/api.js'
 import useAuthUser from './hooks/useAuthUser.js'
 import Layout from './components/Layout.jsx'
+import { useThemeStore } from './store/useThemeStore.js'
 
 const App = () => {
 
+  const {theme} = useThemeStore()
 
   const {isLoading , authUser} = useAuthUser()
   const isAuthenticated = Boolean(authUser)
@@ -26,7 +28,7 @@ const App = () => {
   if(isLoading) return <PageLoader/>
 
   return (
-    <div className='h-screen' data-theme = "forest">
+    <div className='h-screen' data-theme = {theme}>
       <Routes>
         <Route path='/' element={isAuthenticated && isOnBoarded ? 
         <Layout showSideBar={true}>

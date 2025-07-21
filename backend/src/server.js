b6901982445c03d62b769f5d0d72d1f5f34a152b
Harsh Sharma <harshsharma301a@gmail.com>
2025-07-21 11:52:05 +0530

    added changes for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,12 @@ import userRoutes from './routes/user.js'
 import {connectDb} from "./lib/db.js";
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import path from "path"
 
 
 const app = express();
+
+const __dirname = path.resolve()
 app.use(cors({
     origin : "http://localhost:5173",
     credentials : true // allow frontend to send cookies
@@ -18,6 +21,14 @@ app.use(cookieParser())
 app.use('/api/auth' , authRoutes)
 app.use('/api/user' , userRoutes)
 app.use('/api/chat' , chatRoutes)
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname , "../frontend/dist")))
+
+    app.get("*" , (req , res) => {
+        res.sendFile(path.join(__dirname , "../frontend" , "dist" , "index.html"))
+    })
+}
 
 app.listen(PORT , () => {
     console.log(`Server Started On PORT : ${PORT}`)
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
+const BASE_URL = import.meta.env.MODE === "deployment" ? "http://localhost:5001/api" : "/api"
+
 export const axiosInstance = axios.create({
-    baseURL : "http://localhost:5001/api",
+    baseURL : BASE_URL,
     withCredentials : true // this will make sure request will be sent with cookies
 })
